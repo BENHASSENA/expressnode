4d32220f0b5c497e5f8f07c3d4f963d3ef4a9374
Houria BENHASSENA <houriabenhassena@gmail.com>
2021-02-26 14:34:17 +0100

    HouriaBlog

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require ('dotenv/config');
 const express =require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.POST || 3001;
+const PORT = process.env.PORT || 3001;
 
 const mongoose = require('mongoose');
 
